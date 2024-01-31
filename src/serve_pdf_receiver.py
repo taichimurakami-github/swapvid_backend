ec8f2eff5fb8ba176d3f670375e638efe1c58b46
taichi.murakami <tchi0017@gmail.com>
2024-01-31 17:18:30 +0900

    Fix response header to use the origin url

--- src/serve_pdf_receiver.py
+++ src/serve_pdf_receiver.py
@@ -30,7 +30,7 @@ class HttpPostHandler(HttpPostHandlerBase):
     def do_OPTIONS(self):
         self.send_response(200)
         self.send_header("Content-Type", "application/pdf")
-        self.send_header("Access-Control-Allow-Origin", Config().frontend_url)
+        self.send_header("Access-Control-Allow-Origin", self.headers["Origin"])
         self.send_header("Access-Control-Allow-Methods", "POST, OPTIONS")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
         self.end_headers()
